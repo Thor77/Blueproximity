284d862c3f68bf155c9cbd6edc139786f8d1a877
Thor77 <thor77@thor77.org>
2016-08-14 20:22:03 +0200

    Remove ImportError-catch arround imports
    They were basically just a re-raise with a translated message

--- blueproximity/__main__.py
+++ blueproximity/__main__.py
@@ -5,19 +5,8 @@ import os.path
 from xdg import BaseDirectory
 
 import blueproximity.config
+import gtk
 from blueproximity import APP_NAME, Proximity, ProximityGUI
-from blueproximity.translation import _
-
-try:
-    import pygtk
-    pygtk.require("2.0")
-    import gtk
-except ImportError:
-    raise Exception(_('GTK2 bindings for Python are not installed.'))
-try:
-    import gtk.glade
-except ImportError:
-    raise Exception(_('Glade2 bindings for python are not installed.'))
 
 
 def main():
