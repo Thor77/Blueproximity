04e5c3d3144337f490662c1ec0012342f1fcc8b3
Thor77 <thor77@thor77.org>
2016-08-24 22:57:47 +0200

    Add basic cli to specify configpath/activate gui

--- blueproximity/__main__.py
+++ blueproximity/__main__.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import argparse
 import os.path
 
 from xdg import BaseDirectory
@@ -9,18 +10,34 @@ import gtk
 from blueproximity import APP_NAME, Proximity, ProximityGUI
 
 
+def cli():
+    parser = argparse.ArgumentParser(prog='blueproximity', description='''
+        Unlock your computer as soon as a bluetooth-device is in range
+    ''')
+    parser.add_argument('--gui', help='Start GUI', action='store_true')
+    parser.add_argument('-c', '--config', help='Path to configfile', type=str)
+    return parser.parse_args()
+
+
 def main():
+    # cli-args
+    args = cli()
+
     # load config
-    config_path = os.path.join(
-        BaseDirectory.save_config_path(APP_NAME), 'config.ini')
+    if args.config:
+        config_path = args.config
+    else:
+        config_path = os.path.join(
+            BaseDirectory.save_config_path(APP_NAME), 'config.ini')
     configuration = blueproximity.config.load(config_path)
 
     # start Proximity-thread
     proximity = Proximity(configuration)
     proximity.start()
 
-    # start GUI
-    ProximityGUI(configuration)
+    if args.gui:
+        # start GUI
+        ProximityGUI(configuration)
 
     # make GTK threadable
     gtk.gdk.threads_init()
